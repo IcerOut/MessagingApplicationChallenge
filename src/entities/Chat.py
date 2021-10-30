074bf2e4409ebc6fbb6120a8b38bb206e547a9c0
Alin Lung <lung.s.alin@gmail.com>
2021-10-30 14:48:21 +0300

    Simplified Chat entity

--- src/entities/Chat.py
+++ src/entities/Chat.py
@@ -1,31 +1,19 @@
-from typing import List
-
-from entities.Message import Message
-from entities.User import User
-
-
 class Chat:
     """
     A class containing a chat (either group or P2P)
     The CID represents a unique ID for the chat
-    The participants represents a list of User objects, all of the participants of the chat
-    The messages represents a list of Message objects, all of the messages sent in this chat
     """
-    def __init__(self, cid: int or None, participants: List[User] or None,
-                 messages: List[Message] or None):
+
+    def __init__(self, cid: int or None):
         self.cid = cid
-        self.participants = participants
-        self.messages = messages
 
     @classmethod
     def empty(cls):
-        return Chat(None, None, None)
+        return Chat(None)
 
     def __str__(self):
         return 'Chat{' \
-               f'cid={self.cid}, ' \
-               f'participants={self.participants}, ' \
-               f'messages={self.messages}, ' \
+               f'cid={self.cid}' \
                '}'
 
     __repr__ = __str__
