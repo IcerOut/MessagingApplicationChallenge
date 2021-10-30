15210876acff584b66e36386c1e25516b34030f2
Alin Lung <lung.s.alin@gmail.com>
2021-10-30 12:49:23 +0300

    Created aux repository functions that will be used by all 3 repositories

---
+++ src/repo/repository.py
@@ -0,0 +1,82 @@
+import sqlite3
+from contextlib import closing
+from functools import wraps
+
+import CONSTANTS
+
+
+class Repository:
+    @staticmethod
+    def get_connection():
+        return sqlite3.connect(CONSTANTS.DB_FILE_LOCATION, check_same_thread=False)
+
+    @staticmethod
+    def create_if_necessary():
+        """
+        Will open the DB file and create any missing tables, if necessary
+        """
+
+        conn = Repository.get_connection()
+        conn.execute('''
+        CREATE TABLE IF NOT EXISTS Chats
+        (
+            cid INT NOT NULL
+        )''')
+        conn.execute('''
+        CREATE TABLE IF NOT EXISTS Users
+        (
+            username TEXT PRIMARY KEY,
+            password_hash TEXT NOT NULL,
+            authenticated BOOLEAN NOT NULL CHECK(authenticated IN (0, 1))
+        )''')
+        conn.execute('''
+        CREATE TABLE IF NOT EXISTS Messages
+        (
+            sender_username TEXT NOT NULL,
+            destination_chat_id INT NOT NULL
+            timestamp TEXT NOT NULL,
+            MESSAGE TEXT NOT NULL,
+            FOREIGN KEY (sender_username)
+                REFERENCES Users (username)
+                ON DELETE NO ACTION
+                ON UPDATE NO ACTION
+            FOREIGN KEY (destination_chat_id)
+                REFERENCES Chats (cid)
+                ON DELETE CASCADE
+                ON UPDATE NO ACTION
+            PRIMARY KEY(sender_username, destination_chat_id, timestamp)
+        )''')
+        conn.execute('''
+        CREATE TABLE IF NOT EXISTS ChatMembers
+        (
+            chat_id INT NOT NULL,
+            username TEXT NOT NULL
+            FOREIGN KEY (chat_id)
+                REFERENCES Chats (cid)
+                ON DELETE CASCADE
+                ON UPDATE NO ACTION
+            FOREIGN KEY (username)
+                REFERENCES Users (username)
+                ON DELETE CASCADE
+                ON UPDATE NO ACTION
+            PRIMARY KEY(chat_id, username)
+        )''')
+
+    @staticmethod
+    def db_connect(func):
+        """
+        Decorator to guarantee a valid connection object in the decorated function
+        The connection will be closed after the decorated function returns
+        :param Callable func: decorated function
+        """
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if 'connection' not in kwargs:
+                kwargs['connection'] = Repository.get_connection()
+                with closing(kwargs['connection']):
+                    return func(*args, **kwargs)
+            else:
+                return func(*args, **kwargs)
+
+        return wrapper
